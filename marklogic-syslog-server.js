38b01022382108a84d39ba533916a853fb275eb2
Justin Makeig <jmpublic+github@makeig.com>
2014-11-23 16:24:53 -0800

    Naive one write per message.

--- marklogic-syslog-server.js
+++ marklogic-syslog-server.js
@@ -1,6 +1,8 @@
-var dgram = require("dgram");
-var server = dgram.createSocket("udp4");
+var dgram = require('dgram');
+var server = dgram.createSocket('udp4');
 var parser = require('glossy').Parse;
+var uuid = require('node-uuid');
+var marklogic = require('marklogic');
 
 /*
   <http://wiki.splunk.com/Community:HowTo_Configure_Mac_OS_X_Syslog_To_Forward_Data>
@@ -17,20 +19,44 @@ var parser = require('glossy').Parse;
   syslog -s -l DEBUG "Hello, world."
   
   # Query
-  syslog -k Facility | grep -i marklogic
+  syslog -k Sender MarkLogic
   
   Facility 3: daemon
   RFC3164
   
  */
+ 
+ // TODO: Make port number externally configurable
+
+var conn = {
+  host: "localhost",
+  port: 8000,
+  database: "Logs",
+  user: "admin",
+  password: "********",
+  authType: "DIGEST"
+}
+var db = marklogic.createDatabaseClient(conn);
+
 
 //create an event listener for when a syslog message is recieved
 server.on("message", function (msg, rinfo) {
   //sanitise the data by replacing single quotes with two single-quotes
   var message = msg.toString();
-  
   if(message.match(/MarkLogic/g)) {
-    console.log(parser.parse(msg.toString('utf8', 0)));
+    //console.log(parser.parse(msg.toString('utf8', 0)));
+    db.documents.write(
+      {
+        uri: "/" + uuid.v4() + ".json",
+        contentType: "application/json",
+        collections: ["logs"],
+        content: parser.parse(msg.toString('utf8', 0))
+      }
+    ).
+      result(function(response){
+        console.dir(JSON.stringify(response))
+      });
+
   }
 });
 
